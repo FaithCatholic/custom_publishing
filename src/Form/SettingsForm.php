5350e48c8d3eade3d9e25a4a5c3e49daed21b416
Nicholas Ketchum <nketchum@gmail.com>
2020-01-19 17:10:32 -0500

    add role-based and content-based granularity

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -3,8 +3,9 @@
 namespace Drupal\custom_publishing\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
-use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\Entity\Role;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Defines a form that configures custom_publishing settings.
@@ -33,11 +34,38 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     $config = $this->config('custom_publishing.settings');
 
+    $role_options = [];
+    $roles = Role::loadMultiple();
+    foreach ($roles as $role) {
+      $role_options[$role->id()] = $role->label();
+    }
+    $form['roles'] = array(
+      '#title' => $this->t('Roles'),
+      '#type' => 'checkboxes',
+      '#default_value' => $config->get('roles'),
+      '#description' => $this->t('Choose roles to which custom publishing applies.'),
+      '#options' => $role_options,
+      '#required' => FALSE,
+    );
+
+    $type_options = [];
+    $types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
+    foreach ($types as $key => $value) {
+      $type_options[$key] = $value->get('name');
+    }
+    $form['types'] = array(
+      '#title' => $this->t('Content types'),
+      '#type' => 'checkboxes',
+      '#default_value' => $config->get('types'),
+      '#description' => $this->t('Choose which content types to apply custom publishing.'),
+      '#options' => $type_options,
+    );
+
     $form['notify'] = array(
       '#title' => $this->t('Enable email notifications'),
       '#type' => 'checkbox',
       '#default_value' => $config->get('notify') ? 1 : 0,
-      '#description' => $this->t('Enable notifications for anonymously-submitted nodes.'),
+      '#description' => $this->t('Enable notifications for custom publishing nodes.'),
       '#required' => FALSE,
     );
 
